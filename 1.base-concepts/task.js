72aa845569c7759389456b3319a9bd939b08c9e0
Proxergo <rastard@yandex.ru>
2022-06-08 13:30:55 +0300

    Выполнено основное хадачние и со звездочкой

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,33 @@
+'use strict';
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = Math.pow(b, 2) - 4*a*c;
+
+  if (d < 0) {
+    return arr = [];
+  } else if (d === 0) {
+    return arr = [(-b)/(2*a)];
+  } else if (d > 0) {
+    return arr = [(-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+  } 
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  const now = new Date();
+  let n = (date.getFullYear() - now.getFullYear())*12 + Math.abs(date.getMonth() - now.getMonth());
+  let S = amount - contribution;
+  let P = ((percent)/12)/100;
+  console.log(P);
+  let monthlyAmount;
 
-  // код для задачи №2 писать здесь
+  if (P > 0 && P < 1) {
+    monthlyAmount = S*(P + (P/((Math.pow((1 + P) , n)) - 1)));  
+  }
 
-  return totalAmount;
+  console.log(monthlyAmount);
+  
+  totalAmount = monthlyAmount*n;
+  return totalAmount.toFixed(2);
 }
