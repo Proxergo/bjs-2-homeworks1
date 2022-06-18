513c10103db2460a6fa92c8a3a9807b354b0e765
Proxergo <rastard@yandex.ru>
2022-06-18 15:44:49 +0300

    Задание выполнено. Проверка одинаковых id выдает ошибку Jasmine

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,31 +1,27 @@
 
 
 class AlarmClock {
+
     constructor() {
         this.alarmCollection = [];
         this.timerId = null;
     }
+
     addClock(time, callback, id) {
         
         if (id == undefined) {
             throw new Error('Не передан идентификатор таймера!');
-        }
-        
-        if (this.alarmCollection.find(item => item === this.alarmCollection[id])) {
+        } else if (this.alarmCollection.find(item => item.id === id) !== undefined) {
             throw console.error('Такой будильник уже есть!')
+        } else {
+            return this.alarmCollection.push({time, callback, id});
         }
-        
-        this.alarmCollection.push(
-            {time, 
-            callback, 
-            id}
-            );
     }
 
-    removeClock() {
-        this.alarmCollection = this.alarmCollection.filter(item => {return  item !== id});
+    removeClock(id) {
+        this.alarmCollection = this.alarmCollection.filter(item => item.id !== id);
        
-       if (this.alarmCollection=== undefined) {
+       if (this.alarmCollection === undefined) {
             return false;
        } 
 
@@ -47,11 +43,41 @@ class AlarmClock {
 
             if (this.timerId === null) {
                 this.setInterval(() => {
-                    this.alarmCollection.forEach(alarm => checkClock(alarm))
+                    this.alarmCollection.forEach(item => checkClock(item));
                 }, 100);
             }
-        }
-         
-    
+            return;
+        } 
     }
+
+    stop() {
+
+        if (this.timerId !== null) {
+                clearInterval(this.timerId);
+                this.timerId - null;
+        }
+     }
+    
+    printAlarms() {
+        this.alarmCollection.forEach(item => console.log(item.id + ' ' + item.time));
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.timerId = [];
+    }
+}
+
+function testCase() {
+	let clock = new AlarmClock();
+	clock.addClock(clock.getCurrentFormattedTime(), () => console.log('Доброе утро!'),60);
+	let currentDate = new Date();
+	currentDate.setMinutes(currentDate.getMinutes() + 1);
+	clock.addClock(clock.getCurrentFormattedTime(currentDate), () => { console.log('Пора встать!'); clock.removeClock(215) }, 215);
+	currentDate = new Date();
+	currentDate.setMinutes(currentDate.getMinutes() + 2);
+	clock.addClock(clock.getCurrentFormattedTime(currentDate),
+		() => { console.log('Вставай уже! Всё проспишь!'); clock.stop(); clock.clearAlarms() }, 3000);
+	clock.printAlarms();
+	clock.start();
 }
